90a2478f5c16dabcedaeb987dabe0f3fb847dc34
iker-barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2020-04-04 23:20:01 +0200

    Command line support
    Parameters to be used in the video download will now be read from the command line arguments.

--- video_downloader.py
+++ video_downloader.py
@@ -1,11 +1,21 @@
 import requests
 import urllib
 import re
+import argparse
+import sys
+import os
 
 
-FB_VIDEO_URL = ''  # Put here fb video url
+def _add_program_args():
+    arg_parser = argparse.ArgumentParser(description="Downloads videos from Facebook.")
+    arg_parser.add_argument('video_url', type=str, nargs=1, metavar='url', help="The URL of the video to be downloaded.")
+    arg_parser.add_argument('-o', '--output', type=str, nargs='?', default=os.path.abspath(os.path.dirname(sys.argv[0])), help="Output file")
+    arg_parser.add_argument('-q', '--quality', type=str, nargs='?', choices={'sd', 'hd'}, default='hd', help="Quality of the video")
 
-def download_from_facebook(destination_path, url, quality='hd'):
+    return arg_parser.parse_args()
+
+
+def download_video_from_facebook(destination_path, url, quality='hd'):
     # Quality can also be 'sd'
 
     request = requests.get(url)
@@ -21,4 +31,5 @@ def download_from_facebook(destination_path, url, quality='hd'):
 
 
 if __name__ == "__main__":
-    download_from_facebook('videos/fb_test.mp4', FB_VIDEO_URL)
+    args = _add_program_args()
+    download_video_from_facebook(args.output, args.video_url[0], args.quality)
